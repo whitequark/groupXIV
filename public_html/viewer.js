23cae34e9e53e5b3ebccb8f29ce0b090f3499fe8
whitequark <whitequark@whitequark.org>
2018-09-02 08:42:09 +0000

    Make it possible to embed JSON metadata into viewer HTML file.

--- public_html/viewer.js
+++ public_html/viewer.js
@@ -10,7 +10,50 @@ function parseHash() {
   return output;
 }
 
-function initViewer(url, params) {
+function initViewer(options, url) {
+  document.title = options.name + " \u00b7 GroupXIV microphotography viewer"
+
+  var map = GroupXIV({
+    viewport: "viewer",
+    scale: options.scale,
+    tileSize: options.tileSize,
+    layers: options.layers,
+    tilesAlignedTopLeft: options.tilesAlignedTopLeft,
+  });
+
+  function moveMap(params) {
+    if(params.x && params.y && params.z) {
+      var center = map.unproject([parseInt(params.x), parseInt(params.y)], map.getMaxZoom() - 1),
+          zoom = parseInt(params.z);
+      map.setView(center, zoom);
+    }
+  }
+
+  map.on('moveend', function(e) {
+    var center = map.project(map.getCenter(), map.getMaxZoom() - 1),
+        zoom = map.getZoom();
+    var state = "#";
+    if(options["canChangeUrl"]) {
+      state += "url=" + url + "&";
+    }
+    state += "x=" + (center.x|0) + "&";
+    state += "y=" + (center.y|0) + "&";
+    state += "z=" + zoom;
+    history.replaceState(null, null, state);
+  });
+
+  moveMap(parseHash());
+  window.onhashchange = function() {
+    var params = parseHash();
+    if(options["canChangeUrl"] && params["url"] != url) {
+      window.location.reload();
+    } else {
+      moveMap(params);
+    }
+  }
+}
+
+function loadViewer(url) {
   var req = new XMLHttpRequest();
   req.onload = function() {
     var options = JSON.parse(req.responseText);
@@ -18,47 +61,9 @@ function initViewer(url, params) {
       layer.URL = url + "/../" + layer.URL;
       layer.URL = layer.URL.replace(/[^\/]+\/..(\/|$)/, '');
     });
-
-    document.title = options.name + " \u00b7 GroupXIV microphotography viewer"
-
-    var map = GroupXIV({
-      viewport: "viewer",
-      scale: options.scale,
-      tileSize: options.tileSize,
-      layers: options.layers,
-      tilesAlignedTopLeft: options.tilesAlignedTopLeft,
-    });
-
-    function moveMap(params) {
-      if(params.x && params.y && params.z) {
-        var center = map.unproject([parseInt(params.x), parseInt(params.y)], map.getMaxZoom() - 1),
-            zoom = parseInt(params.z);
-        map.setView(center, zoom);
-      }
-    }
-
-    map.on('moveend', function(e) {
-      var center = map.project(map.getCenter(), map.getMaxZoom() - 1),
-          zoom = map.getZoom();
-      history.replaceState(null, null, "#url=" + url +
-        "&x=" + (center.x|0) + "&y=" + (center.y|0) + "&z=" + zoom);
-    });
-
-    moveMap(params);
-    window.onhashchange = function() {
-      var params = parseHash();
-      if(params["url"] != url) {
-        window.location.reload();
-      } else {
-        moveMap(params);
-      }
-    }
+    options["canChangeUrl"] = true;
+    initViewer(options, url);
   };
   req.open("get", url, true);
   req.send();
-}
-
-function pageLoad() {
-  var params = parseHash();
-  initViewer(params["url"], params);
 }
